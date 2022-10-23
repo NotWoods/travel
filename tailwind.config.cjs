c26d27bfd94145078892f989e1b69e8a576a7f32
Tiger Oakes <contact@tigeroakes.com>
2022-10-22 22:21:34 -0700

    Start adding content/styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headings: ["Roboto Serif", "serif"],
+      },
+    },
   },
   plugins: [],
 };
+
+module.exports = config;
