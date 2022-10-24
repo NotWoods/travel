9b14f26d86de9abb2210a84b3b9cbd1fe9784c76
Tiger Oakes <contact@tigeroakes.com>
2022-10-23 21:11:56 -0700

    Cleanup

--- src/pages/rss.xml.js
+++
@@ -1,9 +0,0 @@
-import rss from "@astrojs/rss";
-
-export const get = () =>
-  rss({
-    title: "My personal website.",
-    description: "Welcome to my website!",
-    site: import.meta.env.SITE,
-    items: import.meta.glob("./blog/**/*.{md,mdx}"),
-  });
